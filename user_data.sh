d49e7975f861c196c333581c4a59415423a1aa15
Yousef Ashraf <140858931+yousefkhawagh@users.noreply.github.com>
2025-12-18 09:33:00 +0200

    Update welcome message in index.html

--- user_data.sh
+++ user_data.sh
@@ -3,4 +3,4 @@ dnf update -y
 dnf install nginx -y
 systemctl start nginx
 systemctl enable nginx
-echo "<h1>HELLO FROM ABOELWAFA</h1>" > /usr/share/nginx/html/index.html
+echo "<h1>HELLO FROM Yousef KHAWAGH</h1>" > /usr/share/nginx/html/index.html
